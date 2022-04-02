435000aa792f15e1e787935b8e0a9988d376f9de
Survest <dmitriy.pautov138@gmail.com>
2022-04-02 11:12:42 -0600

    Initial commit

---
+++ app/src/main/java/com/example/shshacksjavaapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.shshacksjavaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
